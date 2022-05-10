a223ca675df47a5f7256db558200b660319e32e9
shubham <agarwal.bit@gmail.com>
2022-05-10 14:48:20 +0530

    update terraform

--- terraform_for_cluster/main.tf
+++ terraform_for_cluster/main.tf
@@ -36,63 +36,63 @@ module "ansible-master"{
   private_ip_address = var.private_ip_address_ansible_master
 }
 
-# resource "azurerm_virtual_machine_extension" "test" {
-#   name                 = "hostname"
-#   virtual_machine_id =  module.ansible-master.vm_id
-#   publisher            = "Microsoft.Azure.Extensions"
-#   type                 = "CustomScript"
-#   type_handler_version = "2.0"
+resource "azurerm_virtual_machine_extension" "test" {
+  name                 = "hostname"
+  virtual_machine_id =  module.ansible-master.vm_id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
 
-#   settings = <<SETTINGS
-#   {
-#      "commandToExecute": "ssh-keygen -t rsa -f ~/.ssh/id_rsa <<< y"
-#   }
-# SETTINGS
+  settings = <<SETTINGS
+  {
+     "commandToExecute": "ssh-keygen -t rsa -f ~/.ssh/id_rsa <<< y"
+  }
+SETTINGS
 
-# }
-#  module "virtual-machine-master" {
-#   depends_on = [
-#     module.vnet
-#   ]
-#   source  = "./modules/vms"
-#   rg_name =   "${var.rg_name}"
-#   location = var.location
-#   subnet_id = module.vnet.subnet_id
-#   vm_name = "${var.vm_name}-master"
-#   vm_size    = var.vm_size_master
-#   ip_allocation_method = var.ip_allocation_method
-#   pub_ip_name = "${var.pub_ip_name}-master"
-#   os_disk_name = "${var.os_disk_name}-master"
-#   computer_name = "${var.computer_name}-master"
-#   private_ip_address = var.private_ip_address_master
-# }
+}
+ module "virtual-machine-master" {
+  depends_on = [
+    module.vnet
+  ]
+  source  = "./modules/vms"
+  rg_name =   "${var.rg_name}"
+  location = var.location
+  subnet_id = module.vnet.subnet_id
+  vm_name = "${var.vm_name}-master"
+  vm_size    = var.vm_size_master
+  ip_allocation_method = var.ip_allocation_method
+  pub_ip_name = "${var.pub_ip_name}-master"
+  os_disk_name = "${var.os_disk_name}-master"
+  computer_name = "${var.computer_name}-master"
+  private_ip_address = var.private_ip_address_master
+}
 
-# module "virtual-machine-node" {
-#   depends_on = [
-#     module.vnet
-#   ]
-#   source  = "./modules/vms"
-#   count = "${var.node_count}"
-#   rg_name =   "${var.rg_name}"
-#   location = "${var.location}"
-#   subnet_id = module.vnet.subnet_id
-#   vm_name = "${var.vm_name}-${count.index}-node"
-#   vm_size    = "${var.vm_size_node}"
-#   ip_allocation_method = "${var.ip_allocation_method}"
-#   pub_ip_name = "${var.pub_ip_name}-${count.index}-node"
-#   os_disk_name = "${var.os_disk_name}-${count.index}-node"
-#   computer_name = "${var.computer_name}-${count.index}-node"
-#   private_ip_address = "${format("10.0.%03d.6", count.index + 1)}"
+module "virtual-machine-node" {
+  depends_on = [
+    module.vnet
+  ]
+  source  = "./modules/vms"
+  count = "${var.node_count}"
+  rg_name =   "${var.rg_name}"
+  location = "${var.location}"
+  subnet_id = module.vnet.subnet_id
+  vm_name = "${var.vm_name}-${count.index}-node"
+  vm_size    = "${var.vm_size_node}"
+  ip_allocation_method = "${var.ip_allocation_method}"
+  pub_ip_name = "${var.pub_ip_name}-${count.index}-node"
+  os_disk_name = "${var.os_disk_name}-${count.index}-node"
+  computer_name = "${var.computer_name}-${count.index}-node"
+  private_ip_address = "${format("10.0.%03d.6", count.index + 1)}"
 
-# }
+}
 
-# resource "local_file" "hosts_cfg" {
-#   content = templatefile("inventory_template",
-#     { 
-#       master_ip_addrs = module.virtual-machine-master.public_ip
-#       ip_addrs = module.virtual-machine-node.*.public_ip
-#     }
-#   )
-#   filename = "inventory"
-# }
+resource "local_file" "hosts_cfg" {
+  content = templatefile("inventory_template",
+    { 
+      master_ip_addrs = module.virtual-machine-master.public_ip
+      ip_addrs = module.virtual-machine-node.*.public_ip
+    }
+  )
+  filename = "inventory"
+}
 
